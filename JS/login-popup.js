dcff27c0a5c5fdfc7477d7b14845febab07c1a2f
Gulnara19 <115493722+Gulnara19@users.noreply.github.com>
2022-10-19 00:23:27 +0300

    добавить файлы

---
+++ JS/login-popup.js
@@ -0,0 +1,8 @@
+ let loginButton = document.querySelector('.login-button');
+ let loginPopup = document.querySelector('.login-popup');
+
+loginButton.addEventListener('click', showPopup);
+function showPopup(evt) {
+    evt.preventDefault();
+    loginPopup.classList.toggle('show-popup');
+}
